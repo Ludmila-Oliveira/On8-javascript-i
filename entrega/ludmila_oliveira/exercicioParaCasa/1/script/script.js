76a5b0bc615fbe60d765a382d6e877a3952aee1b
Ludmila Oliveira <ludsh220@gmail.com>
2020-10-22 16:39:50 -0300

    atualizando o repośitório

--- entrega/ludmila_oliveira/exercicioParaCasa/1/script/script.js
+++ entrega/ludmila_oliveira/exercicioParaCasa/1/script/script.js
@@ -41,7 +41,6 @@ function tratarNumero(elemento) {
 
 // Função para fazer o cálculo da média ----------------------------------------------------
 
-
 botao.addEventListener('click', calcularMedia);
 
 function calcularMedia(event) {
